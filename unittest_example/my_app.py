fbeb388dbebf757623a6ff76ede6a2f7e80f5a67
burapol <taan.burapol@gmail.com>
2023-08-03 16:52:16 +0700

    Example of unittest

--- unittest_example/my_app.py
+++ unittest_example/my_app.py
@@ -1,18 +1,48 @@
 import tkinter as tk
 
 
-class MyApp:
-    def __init__(self, root):
-        self.root = root
-        self.label = tk.Label(root, text="Hello, Tkinter!")
-        self.label.pack()
+class CalculatorApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Calculator")
 
+        self.result_label = tk.Label(self.root, text="Result:")
+        self.result_label.pack()
 
-def main():
-    root = tk.Tk()
-    MyApp(root)
-    root.mainloop()
+        self.entry1 = tk.Entry(self.root)
+        self.entry1.pack()
+
+        self.entry2 = tk.Entry(self.root)
+        self.entry2.pack()
+
+        self.add_button = tk.Button(self.root, text="Add", command=self.add)
+        self.add_button.pack()
+
+        self.sub_button = tk.Button(self.root, text="Subtract", command=self.subtract)
+        self.sub_button.pack()
+
+    def add(self):
+        try:
+            num1 = float(self.entry1.get())
+            num2 = float(self.entry2.get())
+            result = num1 + num2
+            self.result_label.config(text=f"Result: {result}")
+        except ValueError:
+            self.result_label.config(text="Error: Invalid input")
+
+    def subtract(self):
+        try:
+            num1 = float(self.entry1.get())
+            num2 = float(self.entry2.get())
+            result = num1 - num2
+            self.result_label.config(text=f"Result: {result}")
+        except ValueError:
+            self.result_label.config(text="Error: Invalid input")
+
+    def run(self):
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    app = CalculatorApp()
+    app.run()
--- unittest_example/test_my_app.py
+++ unittest_example/test_my_app.py
@@ -1,16 +1,35 @@
 import unittest
 import tkinter as tk
-from tkinter import messagebox
-from my_app import MyApp
+from my_app import CalculatorApp
 
 
-class TestMyApp(unittest.TestCase):
+class TestCalculatorApp(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+        self.app = CalculatorApp()
+        self.app.root = self.root
 
-    def test_my_app_label_text(self):
-        root = tk.Tk()
-        app = MyApp(root)
-        self.assertEqual(app.label.cget("text"), "Hello, Tkinter!")
+    def tearDown(self):
+        self.root.destroy()
+
+    def test_add(self):
+        self.app.entry1.insert(0, "5")
+        self.app.entry2.insert(0, "10")
+        self.app.add()
+        self.assertEqual(self.app.result_label.cget("text"), "Result: 15.0")
+
+    def test_subtract(self):
+        self.app.entry1.insert(0, "10")
+        self.app.entry2.insert(0, "5")
+        self.app.subtract()
+        self.assertEqual(self.app.result_label.cget("text"), "Result: 5.0")
+
+    def test_invalid_input(self):
+        self.app.entry1.insert(0, "Hello")
+        self.app.entry2.insert(0, "World")
+        self.app.add()
+        self.assertEqual(self.app.result_label.cget("text"), "Error: Invalid input")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
